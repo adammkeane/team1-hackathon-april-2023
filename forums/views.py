b0a5b5cd6c85728c1b88ef97609e46583fa97e50
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-14 20:55:29 +0100

    Add create post view

--- forums/views.py
+++ forums/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 from .models import Post
-from django.views.generic.list import ListView
+from django.views import generic
 
 
 class ForumView(TemplateView):
@@ -9,7 +9,7 @@ class ForumView(TemplateView):
     template_name = "forum.html"
 
 
-class PostListView(ListView):
+class PostListView(generic.ListView):
     """List view for displaying all posts"""
 
     template_name = "forum.html"
@@ -17,3 +17,12 @@ class PostListView(ListView):
     context_object_name = "post_list"
     queryset = Post.objects.filter(approved=True).order_by("-created_at")
     paginate_by = 10
+
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = [
+        "title",
+        "content",
+        "category",
+    ]
