6e5cafdd751914821bc35351d7a4deb7c17a784f
adammkeane <adamkeane.edu@gmail.com>
2023-04-14 18:03:02 +0000

    Add comment model and register it with admin

--- forums/admin.py
+++ forums/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -7,3 +7,10 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Comment, CommentAdmin)
