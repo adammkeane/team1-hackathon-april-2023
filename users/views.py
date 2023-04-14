4e1cf867ca20d3ee1918d1aae6b9e2aa0a7c9058
ChrisT-CC <cristaecc@gmail.com>
2023-04-14 23:10:25 +0000

    Add login functionality

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 """ Users app views """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -10,4 +11,21 @@ def sign_up(request):
 
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        """
+        Code from Django docs
+        https://docs.djangoproject.com/en/3.2/topics/auth/default/#auth-web-requests
+        lines 19-23
+        """
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("Login error. Try again..."))
+            # messages.success(request, ("Login error. Try again..."))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
