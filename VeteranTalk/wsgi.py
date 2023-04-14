9165d96a740d77ccc5794e9a3838e1efe4544ff3
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-14 11:23:12 +0100

    Rename project to VeteranTalk

--- VeteranTalk/wsgi.py
+++ VeteranTalk/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for team1hackathon project.
+WSGI config for VeteranTalk project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team1hackathon.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VeteranTalk.settings")
 
 application = get_wsgi_application()
