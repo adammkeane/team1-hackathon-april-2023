6e49a19ad8bc5055dfe2dab460b33771f66e959c
adammkeane <adamkeane.edu@gmail.com>
2023-04-14 17:46:58 +0000

    Add profile model and register it with admin

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.TextField(blank=True, max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    bio = models.TextField(blank=True, max_length=4000)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.owner)
