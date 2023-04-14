3f690ee877dc078fbc0319a6ffa5f5fa2c5ad15b
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-14 11:17:08 +0100

    Remove chatsupport app

--- chatsupport/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatsupportConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "chatsupport"
--- team1hackathon/settings.py
+++ team1hackathon/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "cloudinary",
     "livereload",
-    "chatsupport",
 ]
 
 MIDDLEWARE = [
