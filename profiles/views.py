2b3b87b4ad5e07e6c56dbcd4ef34ecbf85b962e2
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-15 15:33:29 +0100

    Add user posts to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,20 +1,51 @@
 from django.views import generic
 from django.contrib.auth.models import User
+from django.views.generic import TemplateView
+from forums.models import Post
+from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+
+# class ProfileView(generic.ListView):
+#     """Profile page view"""
+
+#     model = User
+#     template_name = "profile.html"
+
+#     def get_queryset(self):
+#         User.objects.filter(username=self.kwargs.get("id"))
 
 
-class ProfileView(generic.ListView):
-    """Profile page view"""
+class ProfileView(generic.View):
+    def get(self, request, id, *args, **kwargs):
+        current_user = get_object_or_404(User.objects, id=id)
+        posts = current_user.post_owner.order_by("-created_at")
 
-    model = User
-    template_name = "profile.html"
+        return render(
+            request,
+            "profile.html",
+            {
+                "current_user": current_user,
+                "posts": posts,
+            },
+        )
 
-    def get_queryset(self):
-        User.objects.filter(username=self.kwargs.get("id"))
 
+class PostDetailView(generic.View):
+    def get(self, request, id, *args, **kwargs):
+        post = get_object_or_404(Post.objects, id=id)
+        comments = post.post_comment.order_by("-created_at")
+        if len(comments) > 0:
+            no_comments = False
+        else:
+            no_comments = True
 
-# from django.shortcuts import render
-# from django.contrib.auth.models import User
-
-# def profile_view(request):
-#     current_user = request.user
-#     posts = request.g
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "no_comments": no_comments,
+            },
+        )
