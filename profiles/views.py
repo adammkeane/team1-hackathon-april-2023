056453f2c5af2e3891a9f4ca3152e411ee8e95dc
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-15 14:50:01 +0100

    Add url to load current user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,20 @@
-from django.views.generic import TemplateView
+from django.views import generic
+from django.contrib.auth.models import User
 
 
-class ProfileView(TemplateView):
+class ProfileView(generic.ListView):
     """Profile page view"""
 
+    model = User
     template_name = "profile.html"
+
+    def get_queryset(self):
+        User.objects.filter(username=self.kwargs.get("id"))
+
+
+# from django.shortcuts import render
+# from django.contrib.auth.models import User
+
+# def profile_view(request):
+#     current_user = request.user
+#     posts = request.g
