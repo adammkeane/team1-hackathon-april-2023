3da2f078e8066d3bbc08ab693d83c490cb2d2375
Tom Ainsworth <tomainsworth94@gmail.com>
2023-04-15 16:50:06 +0100

    Add create post view and template

--- forums/urls.py
+++ forums/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
-    path("post/<slug:id>/comment/", views.CommentCreateView.as_view(), name="comment_create"),
-    path("post/<slug:id>/", views.PostDetailView.as_view(), name='post_detail'),
+    path(
+        "post/<slug:id>/comment/",
+        views.CommentCreateView.as_view(),
+        name="comment_create",
+    ),
+    path("post/<slug:id>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("post_create/", views.PostCreateView.as_view(), name="post_detail"),
 ]
